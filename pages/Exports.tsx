bb15e5639983e22d2c9a2fccf61976a616a6992f
kibutan <s1170230@gmail.com>
2022-07-04 16:00:00 +0900

    Muriyari build

--- pages/Exports.tsx
+++ pages/Exports.tsx
@@ -1,3 +1,7 @@
 export { default as FixedDistortions } from "./components/FixedDistortions";
 export { default as MyBox } from "./components/MyBox";
 export { default as SelectItems } from "./components/SelectItems";
+
+export default function Exports() {
+  return <div>Exports</div>;
+}
